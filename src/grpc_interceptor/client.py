d10b7e282246947062cd28589c5c7ac158f6285a
villainy <git@morgan83.com>
2020-12-05 14:21:54 -0500

    Use namedtuples to support 3.6

--- src/grpc_interceptor/client.py
+++ src/grpc_interceptor/client.py
@@ -1,36 +1,43 @@
 """Base class for client-side interceptors."""
 
 import abc
-from dataclasses import dataclass, field
-from typing import Any, Callable, Iterator, List, Optional, Tuple
+from collections import namedtuple
+from typing import Any, Callable, Iterator, Optional, Tuple
 
 import grpc
 
 
-@dataclass
-class ClientCallDetails(grpc.ClientCallDetails):
+class ClientCallDetails(
+    namedtuple(
+        "ClientCallDetails",
+        (
+            "method",
+            "timeout",
+            "metadata",
+            "credentials",
+            "wait_for_ready",
+            "compression",
+        ),
+    ),
+    grpc.ClientCallDetails,
+):
     """Describes an RPC to be invoked.
 
     This is an EXPERIMENTAL API.
 
     Attributes:
-    method: The method name of the RPC.
-    timeout: An optional duration of time in seconds to allow for the RPC.
-    metadata: Optional :term:`metadata` to be transmitted to
-        the service-side of the RPC.
-    credentials: An optional CallCredentials for the RPC.
-    wait_for_ready: This is an EXPERIMENTAL argument. An optional
-            flag to enable :term:`wait_for_ready` mechanism.
-    compression: An element of grpc.compression, e.g.
-        grpc.compression.Gzip. This is an EXPERIMENTAL option.
+        method: The method name of the RPC.
+        timeout: An optional duration of time in seconds to allow for the RPC.
+        metadata: Optional :term:`metadata` to be transmitted to the
+                  service-side of the RPC.
+        credentials: An optional CallCredentials for the RPC.
+        wait_for_ready: This is an EXPERIMENTAL argument. An optional flag to
+                        enable :term:`wait_for_ready` mechanism.
+        compression: An element of grpc.compression, e.g. grpc.compression.Gzip.
+                     This is an EXPERIMENTAL option.
     """
 
-    method: str
-    timeout: Optional[int] = field(default=None)
-    metadata: List[Tuple[str, str]] = field(default_factory=list)
-    credentials: Optional[grpc.CallCredentials] = field(default=None)
-    wait_for_ready: Optional[bool] = field(default=None)
-    compression: Optional[grpc.Compression] = field(default=None)
+    pass
 
 
 class ClientInterceptor(
