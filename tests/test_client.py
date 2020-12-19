b5f312171585790ebfb74e806c05cf6b70297811
villainy <git@morgan83.com>
2020-12-19 10:47:43 -0500

    Update tests
    Update metadata interceptor tests for client refactors.
    Postprocessors no longer exist, remove tests.

--- tests/test_client.py
+++ tests/test_client.py
@@ -14,9 +14,7 @@ class MetadataInterceptor(ClientInterceptor):
     def __init__(self, metadata: List[Tuple[str, str]]):
         self._metadata = metadata
 
-    def intercept(
-        self, call_details, request_iterator, request_streaming, response_streaming
-    ):
+    def intercept(self, method, request_or_iterator, call_details):
         """Add invocation metadata to request."""
         new_details = ClientCallDetails(
             call_details.method,
@@ -27,39 +25,7 @@ class MetadataInterceptor(ClientInterceptor):
             call_details.compression,
         )
 
-        return new_details, request_iterator, None
-
-
-class PostprocessInterceptor(ClientInterceptor):
-    """A test interceptor that logs results."""
-
-    def __init__(self):
-        import logging
-
-        self._log = logging.getLogger(__name__)
-
-    def intercept(
-        self, call_details, request_iterator, request_streaming, response_streaming
-    ):
-        """Log response data."""
-
-        def _unary_logger(outcome):
-            self._log.warning(outcome.result().output)
-            return outcome
-
-        def _stream_logger(response_iterator):
-            collected = ""
-            for response in response_iterator:
-                collected += response.output
-                yield response
-
-            self._log.warning(collected)
-
-        postprocess = _unary_logger
-        if response_streaming:
-            postprocess = _stream_logger
-
-        return call_details, request_iterator, postprocess
+        return method(new_details, request_or_iterator)
 
 
 @pytest.fixture
@@ -74,13 +40,13 @@ def metadata_client():
     intr = MetadataInterceptor([("this_key", "this_value")])
     interceptors = [intr]
 
-    context_cases = {
-        "metadata": lambda c: ",".join(
+    special_cases = {
+        "metadata": lambda _, c: ",".join(
             f"{key}:{value}" for key, value in c.invocation_metadata()
         )
     }
     with dummy_client(
-        special_cases={}, context_cases=context_cases, client_interceptors=interceptors
+        special_cases=special_cases, client_interceptors=interceptors
     ) as client:
         yield client
 
@@ -118,51 +84,3 @@ def test_metadata_client_server_stream(metadata_client, metadata_string):
     result = metadata_client.ExecuteClientServerStream(stream_stream_input)
     stream_stream_output = [r.output for r in result]
     assert metadata_string in stream_stream_output[0]
-
-
-@pytest.fixture
-def logged_string():
-    """Expected logged response string."""
-    return "logthisthing"
-
-
-@pytest.fixture
-def postprocess_client():
-    """Client with postprocess interceptor."""
-    intr = PostprocessInterceptor()
-    interceptors = [intr]
-
-    with dummy_client(
-        special_cases={}, context_cases={}, client_interceptors=interceptors
-    ) as client:
-        yield client
-
-
-def test_postprocess_unary(caplog, postprocess_client, logged_string):
-    """Response output field should be logged."""
-    postprocess_client.Execute(DummyRequest(input=logged_string))
-    assert logged_string in caplog.text
-
-
-def test_postprocess_server_stream(caplog, postprocess_client, logged_string):
-    """Response output fields should be concatenated and logged."""
-    for _ in postprocess_client.ExecuteServerStream(DummyRequest(input=logged_string)):
-        pass
-
-    assert logged_string in caplog.text
-
-
-def test_postprocess_client_stream(caplog, postprocess_client, logged_string):
-    """Response output field should be logged."""
-    client_stream_input = iter((DummyRequest(input=logged_string),))
-    postprocess_client.ExecuteClientStream(client_stream_input)
-    assert logged_string in caplog.text
-
-
-def test_postprocess_client_server_stream(caplog, postprocess_client, logged_string):
-    """Response output fields should be concatenated and logged."""
-    stream_stream_input = iter((DummyRequest(input=logged_string),))
-    for _ in postprocess_client.ExecuteClientServerStream(stream_stream_input):
-        pass
-
-    assert logged_string in caplog.text
