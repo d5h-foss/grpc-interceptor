64110bbbd8c5810493847ae90ef65c537eeb68cd
villainy <git@morgan83.com>
2020-12-19 10:19:53 -0500

    Refactor interceptor to take continuation directly
    Pass continuation, call details, and request or iterator directly to
    interceptor implementation.
    This gives the implementor full control over how the requested RPC is
    called.

--- src/grpc_interceptor/client.py
+++ src/grpc_interceptor/client.py
@@ -21,31 +21,32 @@ class ClientInterceptor(
     @abc.abstractmethod
     def intercept(
         self,
+        continuation: Callable,
         call_details: grpc.ClientCallDetails,
-        request_iterator: Iterator[Any],
-        request_streaming: bool,
-        response_streaming: bool,
+        request_or_iterator: Any,
     ) -> Tuple[grpc.ClientCallDetails, Iterator[Any], Optional[Callable]]:
         """Override this method to implement a custom interceptor.
 
-        This method is called for all unary and streaming RPCs with the
-        appropriate boolean parameters set. The returned
-        grpc.ClientCallDetails and request message(s) will be passed to
-        either the next interceptor or RPC implementation. An optional
-        callback function can be returned to perform postprocessing on RPC
-        responses.
+        This method is called for all unary and streaming RPCs. The
+        `request_or_iterator` parameter should be type checked to determine if this
+        is a singluar request for unary RPCs or an iterator for client-streaming or
+        client-server streaming RPCs. The interceptor implementation should call
+        `continuation` using a `grpc.ClientCallDetails` and the `request_or_iterator`
+        object as parameters.
 
         Args:
+            continuation (Callable): A function that proceeds with the invocation by
+            executing the next interceptor in chain or invoking the actual RPC on the
+            underlying Channel.
             call_details (grpc.ClientCallDetails): Describes an RPC to be invoked
-            request_iterator (Iterator[Any]): RPC request messages
-            request_streaming (bool): True if RPC is client or bi-directional streaming
-            response_streaming (bool): True if PRC is server or bi-directional streaming
+            request_or_iterator (Any): RPC request message(s)
 
         Returns:
-            This should return a tuple of grpc.ClientCallDetails, RPC request
-            message iterator, and a postprocessing callback function or None.
+            The return for an interceptor should match the return interface for a
+            continuation. This is an object that is both a Call for the RPC and a
+            Future.
         """
-        return call_details, request_iterator, None  # pragma: no cover
+        return continuation(call_details, request_or_iterator)  # pragma: no cover
 
     def intercept_unary_unary(
         self,
@@ -58,16 +59,7 @@ class ClientInterceptor(
         This is not part of the grpc_interceptor.ClientInterceptor API, but must have
         a public name. Do not override it, unless you know what you're doing.
         """
-        new_details, new_request_iterator, postprocess = self.intercept(
-            call_details=call_details,
-            request_iterator=iter((request,)),
-            request_streaming=False,
-            response_streaming=False,
-        )
-        response = continuation(new_details, next(new_request_iterator))
-
-        if postprocess:
-            return postprocess(response)
+        response = self.intercept(continuation, call_details, request)
 
         return response
 
@@ -82,18 +74,9 @@ class ClientInterceptor(
         This is not part of the grpc_interceptor.ClientInterceptor API, but must have
         a public name. Do not override it, unless you know what you're doing.
         """
-        new_details, new_request_iterator, postprocess = self.intercept(
-            call_details=call_details,
-            request_iterator=iter((request,)),
-            request_streaming=False,
-            response_streaming=True,
-        )
-        response_iterator = continuation(new_details, next(new_request_iterator))
+        response = self.intercept(continuation, call_details, request)
 
-        if postprocess:
-            return postprocess(response_iterator)
-
-        return response_iterator
+        return response
 
     def intercept_stream_unary(
         self,
@@ -106,16 +89,7 @@ class ClientInterceptor(
         This is not part of the grpc_interceptor.ClientInterceptor API, but must have
         a public name. Do not override it, unless you know what you're doing.
         """
-        new_details, new_request_iterator, postprocess = self.intercept(
-            call_details=call_details,
-            request_iterator=request_iterator,
-            request_streaming=True,
-            response_streaming=False,
-        )
-        response = continuation(new_details, new_request_iterator)
-
-        if postprocess:
-            return postprocess(response)
+        response = self.intercept(continuation, call_details, request_iterator)
 
         return response
 
@@ -130,15 +104,6 @@ class ClientInterceptor(
         This is not part of the grpc_interceptor.ClientInterceptor API, but must have
         a public name. Do not override it, unless you know what you're doing.
         """
-        new_details, new_request_iterator, postprocess = self.intercept(
-            call_details=call_details,
-            request_iterator=request_iterator,
-            request_streaming=True,
-            response_streaming=True,
-        )
-        response_iterator = continuation(new_details, new_request_iterator)
+        response = self.intercept(continuation, call_details, request_iterator)
 
-        if postprocess:
-            return postprocess(response_iterator)
-
-        return response_iterator
+        return response
